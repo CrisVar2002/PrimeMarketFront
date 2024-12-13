634e204ff1eb571e18dd469e80b9daa4f8042632
Cristian <cvargasluizaga@safareyes.es>
2024-12-13 11:46:20 +0100

    Login y register Terminado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
+import {HomePage} from "./home/home.page";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomePage },
+
+
+
 ];
